4cabb32a6e851f46012192f7a02642fa54918001
wolfgunblood <jalajdorai1@gmail.com>
2023-06-14 21:35:36 +0530

    Completed NavBar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import Display from "../../assets/images/image-hero-desktop.png"
+import "./Hero.scss";
+import Display from "../../assets/images/image-hero-desktop.png";
+import Client1 from "../../assets/images/client-audiophile.svg";
+import Client2 from "../../assets/images/client-databiz.svg";
+import Client3 from "../../assets/images/client-maker.svg";
+import Client4 from "../../assets/images/client-meet.svg";
 
 const Hero = () => {
+  const clients = [Client1, Client2, Client3, Client4];
   return (
     <div className='hero-container'>
-      <div>
-
+      <div className='hero-body'>
+        <div className='hero-content'>
+          <h1>Make <br /> remote work</h1>
+          <p>Get your team in sync, no matter your location. Streamline processes,
+            create team rituals, and watch productivity soar.</p>
+          <button className='btn'>Learn More</button>
+        </div>
+        <div className='client-container'>
+          {clients.map((client, key) => (
+            <img src={client} key={key} />
+          ))}
+        </div>
       </div>
       <div className='img-container'>
         <img src={Display} alt="Hero Image" />
