b42e2647e10e7e79fabd9dd1e5f4935c6377b3d6
wolfgunblood <jalajdorai1@gmail.com>
2023-06-30 19:43:05 +0530

    Added Dropdown Component

--- src/components/Dropdown/DropdownComponent.jsx
+++ src/components/Dropdown/DropdownComponent.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import "./DropdownComponent.scss";
+import Todo from "../../assets/images/icon-todo.svg"
+import Calendar from "../../assets/images/icon-calendar.svg"
+import Reminder from "../../assets/images/icon-reminders.svg"
+import Planning from "../../assets/images/icon-planning.svg"
 
-const DropdownComponent = () => {
+const DropdownComponent = ({ index }) => {
   return (
-    <div>DropdownComponent</div>
+    <>
+      {
+        index == 1 &&
+        <ul className='dropdown-list'>
+          <li className='dropdown-item'><span><img src={Todo} alt="Todo" /></span>Todo List</li>
+          <li className='dropdown-item'><span><img src={Calendar} alt="Calendar" /></span>Calender</li>
+          <li className='dropdown-item'><span><img src={Reminder} alt="Reminder" /></span>Reminders</li>
+          <li className='dropdown-item'><span><img src={Planning} alt="Planning" /></span>Planning</li>
+        </ul>
+      }
+      {
+        index == 2 &&
+        <ul className='dropdown-list'>
+          <li className='dropdown-item'>History</li>
+          <li className='dropdown-item'>Our Team</li>
+          <li className='dropdown-item'>Blog</li>
+        </ul>
+      }
+    </>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navbar.scss"
-import Logo from "../../assets/images/logo.svg"
+import Logo from "../../assets/images/logo.svg";
+import Down from "../../assets/images/icon-arrow-down.svg"
+import Up from "../../assets/images/icon-arrow-up.svg"
+import DropdownComponent from '../Dropdown/DropdownComponent';
 const Navbar = () => {
+  const [drop1, setDrop1] = useState(false)
+  const [drop2, setDrop2] = useState(false)
+
+  // console.log(drop.one)
+  // console.log(drop.two)
+
   return (
     <div className='nav-container'>
       <div className='nav-right'>
         <div className='Logo'>
           <img src={Logo} alt="Logo" />
         </div>
-        <ul>
-          <li>Features</li>
-          <li>Company</li>
-          <li>Careers</li>
-          <li>About</li>
+        <ul className='nav-list'>
+          <li className='nav-item'>
+            <button className='nav-item-btn' onClick={() => setDrop1(!drop1)}>
+              Features
+              <span>
+                {!drop1 && <img src={Down} alt="Down" />}
+                {drop1 && < img src={Down} alt="Down" /> }
+              </span>
+            </button>
+            {drop1 && <DropdownComponent index={1} />}
+          </li>
+          <li className='nav-item'>
+            <button className='nav-item-btn' onClick={() => setDrop2(!drop2)}>
+              Company
+              <span>
+                {!drop2 && <img src={Down} alt="Down" />}
+                {drop2 && < img src={Down} alt="Down" /> }
+              </span>
+            </button>
+            {drop2 && <DropdownComponent index={2} />}
+          </li>
+          <li className='nav-item'>Careers</li>
+          <li className='nav-item'>About</li>
         </ul>
       </div>
       <div className='nav-right'>
